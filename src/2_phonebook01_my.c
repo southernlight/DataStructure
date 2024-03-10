a794301244fffd775b87fc1bd8c6801baa6d375c
Namhoon Kim <southernlight3816@gmail.com>
2024-03-10 22:04:32 +0900

    add dynamic allocation and tokeninzing for wrong command

--- src/2_phonebook01_my.c
+++ src/2_phonebook01_my.c
@@ -1,35 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define CAPACITY 10
 #define INIT_CAPACITY 3
 #define BUFFER_SIZE 50
 
-char **names;   /* names */
-char **numbers; /* phone numbers */
+char **names;
+char **numbers;
 
-int capacity = INIT_CAPACITY;
-int n = 0; /* number of people in phone directory */
+int capacity = INIT_CAPACITY; /* size of array */
+int n = 0;                    /* number of people in phone directory */
+
 char delim[] = " ";
-char *token;
-char command[BUFFER_SIZE];
 
+// char command[BUFFER_SIZE];
+
+/* function prototypes */
+
+// functions in main.
 void init_directory ();
 void process_command ();
 
-void load ();
-void add ();
-void find ();
+// functions for command
+void load (char *fileName);
+void add (char *name, char *number);
+void find (char *name);
 void status ();
 void delete ();
-void save ();
-void sort (char *names[]);
+void save (char *fileName);
+
 int search (char *name);
 int read_line (char str[], int limit);
+void sort (char *names[]);
+void reallocate ();
 
 int
 main ()
@@ -43,130 +48,140 @@ main ()
 void
 init_directory ()
 {
-  names = (char **)malloc (capacity * sizeof (char *));
-  numbers = (char **)malloc (capacity * sizeof (char *));
+  names = (char **)malloc (INIT_CAPACITY * sizeof (char *));
+  numbers = (char **)malloc (INIT_CAPACITY * sizeof (char *));
 }
 
 void
 process_command ()
 {
+  char command_line[BUFFER_SIZE];
+  char *command, *argument1, *argument2;
+
   while (1)
     {
       printf ("$ ");
-      read_line (command, BUFFER_SIZE);
+      if (read_line (command_line, BUFFER_SIZE) <= 0)
+        continue;
 
-      token = strtok (command, delim);
+      command = strtok (command_line, delim);
 
-      if (strcmp (token, "read") == 0)
-        load ();
-      else if (strcmp (token, "add") == 0)
-        add ();
-      else if (strcmp (token, "find") == 0)
-        find ();
-      else if (strcmp (token, "status") == 0)
+      if (strcmp (command, "read") == 0)
+        {
+          argument1 = strtok (NULL, delim);
+          if (argument1 == NULL)
+            {
+              printf ("File name required\n");
+              continue;
+            }
+          load (argument1);
+        }
+
+      else if (strcmp (command, "add") == 0)
+        {
+          argument1 = strtok (NULL, delim);
+          argument2 = strtok (NULL, delim);
+          if (argument1 == NULL || argument2 == NULL)
+            {
+              printf ("Invalid arguments\n");
+              continue;
+            }
+
+          add (argument1, argument2);
+        }
+
+      else if (strcmp (command, "find") == 0)
+        {
+          argument1 = strtok (NULL, delim);
+          if (argument1 == NULL)
+            {
+              printf ("Invalid arguments\n");
+              continue;
+            }
+
+          find (argument1);
+        }
+
+      else if (strcmp (command, "status") == 0)
         status ();
-      else if (strcmp (token, "delete") == 0)
-        delete ();
-      else if (strcmp (token, "save") == 0)
-        save ();
-      else if (strcmp (token, "exit") == 0)
+
+      else if (strcmp (command, "delete") == 0)
+        {
+          argument1 = strtok (NULL, delim);
+          if (argument1 == NULL)
+            {
+              printf ("Invalid arguments\n");
+              continue;
+            }
+
+          delete (argument1);
+        }
+
+      else if (strcmp (command, "save") == 0)
+        {
+          argument1 = strtok (NULL, delim);
+          argument2 = strtok (NULL, delim);
+          if (argument1 == NULL || strcmp (argument1, "as") != 0
+              || argument2 == NULL)
+            {
+              printf ("Invalid command format.\n");
+              continue;
+            }
+
+          save (argument2);
+        }
+
+      else if (strcmp (command, "exit") == 0)
         break;
     }
 }
 
 void
-load ()
+load (char *fileName)
 {
-  int count = 0;
 
-  char file_name[BUFFER_SIZE], name[BUFFER_SIZE], number[BUFFER_SIZE];
-
-  while ((token = strtok (NULL, delim)) != NULL)
-    {
-      count++;
-      if (count == 1)
-        strcpy (file_name, token);
-    }
-
-  if (count < 1)
-    printf ("File name required\n");
-
-  else if (count == 1)
-    {
-      FILE *fp = fopen (file_name, "r");
-      if (fp == NULL)
-        {
-          printf ("Open failed.\n");
-          return;
-        }
-
-      while ((fscanf (fp, "%s %s", name, number)) != EOF)
-        {
-          n++;
-        }
-
-      // sort(names);
-
-      names = (char **)malloc (n * sizeof (char *));
-      numbers = (char **)malloc (n * sizeof (char *));
-
-      for (int i = 0; i < n; ++i)
-        {
-          *(names + i) = _strdup (name);
-          *(numbers + i) = _strdup (number);
-        }
-
-      fclose (fp);
-    }
-
-  else
-    printf ("Too many arguments\n");
-}
-
-void
-add ()
-{
-  int count = 0;
-
-  char name[BUFFER_SIZE], number[BUFFER_SIZE];
-
-  // scanf("%s %s", name, number);
-  while ((token = strtok (NULL, delim)) != NULL)
-    {
-      count++;
-      if (count == 1)
-        strcpy (name, token);
-      else if (count == 2)
-        strcpy (number, token);
-    }
-
-  if (count == 2)
-    {
-      int i = n - 1;
-      while (i >= 0 && strcmp (names[i], name) > 0)
-        {
-          names[i + 1] = names[i];
-          numbers[i + 1] = numbers[i];
-          i--;
-        }
-
-      names[i + 1] = _strdup (name);
-      numbers[i + 1] = _strdup (number);
-
-      n++;
-      printf ("%s was added successfully\n", name);
-    }
-
-  else
-    printf ("Invalid arguments\n");
-}
-
-void
-find ()
-{
   char name[BUFFER_SIZE];
-  scanf ("%s", name);
+  char number[BUFFER_SIZE];
 
+  FILE *fp = fopen (fileName, "r");
+  if (fp == NULL)
+    {
+      printf ("Open failed.\n");
+      return;
+    }
+
+  while ((fscanf (fp, "%s %s", name, number)) != EOF)
+    {
+      add (name, number);
+    }
+
+  fclose (fp);
+}
+
+void
+add (char *name, char *number)
+{
+  if (n >= capacity)
+    reallocate ();
+
+  int i = n - 1;
+  while (i >= 0 && strcmp (names[i], name) > 0)
+    {
+      names[i + 1] = names[i];
+      numbers[i + 1] = numbers[i];
+      i--;
+    }
+
+  names[i + 1] = _strdup (name);
+  numbers[i + 1] = _strdup (number);
+
+  n++;
+  printf ("%s was added successfully\n", name);
+}
+
+void
+find (char *name)
+{
   int index = search (name);
 
   if (index == -1)
@@ -183,10 +198,8 @@ status ()
   printf ("Total %d persons.\n", n);
 }
 
-void delete ()
+void delete (char *name)
 {
-  char name[BUFFER_SIZE];
-  scanf ("%s", name);
   int index = search (name);
   if (index == -1)
     printf ("No person named %s exists.\n", name);
@@ -196,7 +209,6 @@ void delete ()
     {
       names[j] = names[j + 1];
       numbers[j] = numbers[j + 1];
-      // printf("%d\n", j);
     }
 
   n--;
@@ -204,45 +216,23 @@ void delete ()
 }
 
 void
-save ()
+save (char *fileName)
 {
-  int count = 0;
 
-  char file_name[BUFFER_SIZE];
-  char tmp[BUFFER_SIZE];
-
-  // scanf("%s", tmp);
-  // scanf("%s", file_name);
-
-  while ((token = strtok (NULL, delim)) != NULL)
+  FILE *fp = fopen (fileName, "w");
+  if (fp == NULL)
     {
-      count++;
-      if (count == 1)
-        strcpy (tmp, token);
-      else if (count == 2)
-        strcpy (file_name, token);
+      printf ("Open failed");
+      return;
     }
 
-  if (count == 2 && (strcmp (tmp, "as") == 0))
+  for (int i = 0; i < n; i++)
     {
-      FILE *fp = fopen (file_name, "w");
-      if (fp == NULL)
-        {
-          printf ("Open failed");
-          return;
-        }
-
-      for (int i = 0; i < n; i++)
-        {
-          fprintf (fp, "%s ", names[i]);
-          fprintf (fp, "%s\n", numbers[i]);
-        }
-
-      fclose (fp);
+      fprintf (fp, "%s ", names[i]);
+      fprintf (fp, "%s\n", numbers[i]);
     }
 
-  else
-    printf ("Invalid command format.\n");
+  fclose (fp);
 }
 
 void
@@ -304,5 +294,26 @@ read_line (char str[], int limit)
       str[i++] = ch;
 
   str[i] = '\0';
+
   return i;
+}
+
+void
+reallocate ()
+{
+  capacity *= 2;
+  char **new_names = (char **)malloc (capacity * sizeof (char *));
+  char **new_numbers = (char **)malloc (capacity * sizeof (char *));
+
+  for (int i = 0; i < n; i++)
+    {
+      new_names[i] = names[i];
+      new_numbers[i] = numbers[i];
+    }
+
+  free (names);
+  free (numbers);
+
+  names = new_names;
+  numbers = new_numbers;
 }
